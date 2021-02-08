094cd87cfef7bb682ace5dd3b879af93499f5ef7
gabida <gabida.steam@gmail.com>
2021-02-08 01:58:57 +0100

    crud working

---
+++ src/app/model/category.model.ts
@@ -0,0 +1,6 @@
+export class Category{
+    public  categorieId: number;
+	public catName: string;
+	
+
+}
---
+++ src/app/model/product.model.ts
@@ -0,0 +1,14 @@
+import { Category } from "./category.model";
+
+
+
+export class Product {
+   public produitId:number;
+   public label:string;
+
+   public prix : number;
+
+  
+   public category: Category;
+   
+}
